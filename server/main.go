9c044f492f617aa8f60ca458f3e1dea8b231cf2a
Ritwik Saha <ritwiksahaofficial@gmail.com>
2019-03-22 08:33:20 +0530

    Reading App Secrets on Server

--- server/main.go
+++ server/main.go
@@ -2,44 +2,25 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
+var isDev bool        // Is in development mode
+var mySecrets Secrets // mySecrets
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/view/", viewHandler)
+	getSecrets(isDev, &mySecrets)
 
+	fmt.Println("isDev", isDev)
+
+	fmt.Printf("%+v\n", mySecrets)
+	fmt.Println(mySecrets.Port)
+
+	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ server/secrets.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// Secrets ...
+type Secrets struct {
+	Port  int    `json:"port"`
+	Mystr string `json:"mystr"`
+}
+
+// Get Secrets for App
+func getSecrets(isDev bool, mySecrets *Secrets) {
+	isDev = os.Getenv("isDev") == "true"
+
+	var filename string // JSON file location
+
+	if isDev {
+		filename = "config/config.development.json"
+	} else {
+		filename = "config/config.production.json"
+	}
+
+	jsonFile, readErr := os.Open(filename)
+
+	if readErr != nil {
+		fmt.Println("Error", readErr)
+	}
+
+	fmt.Println("Successfully Opened config json")
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	marshErr := json.Unmarshal([]byte(byteValue), &mySecrets)
+
+	if marshErr != nil {
+		fmt.Println("Error", marshErr)
+	}
+}
